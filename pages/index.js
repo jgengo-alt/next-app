061151c3e718e323dbc28fbbc2615c0806e02a1e
Jordane Gengo <jordane@hive.fi>
2022-03-27 11:45:29 +0300

    init project

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>next-app | Home</title>
+			</Head>
+
+			<p>This is a sample website - deployed with Github actions</p>
+
+		</div>
+
+	)
+}
